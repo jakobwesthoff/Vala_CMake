285255b42ad8a88993d8fe40ebb209cbe87ec4b8
Jakob Westhoff <jakob@westhoffswelt.de>
2010-08-24 01:21:54 +0200

    Bumped copyright years

--- vala/ValaPrecompile.cmake
+++ vala/ValaPrecompile.cmake
@@ -1,5 +1,5 @@
 ##
-# Copyright 2009 Jakob Westhoff. All rights reserved.
+# Copyright 2009-2010 Jakob Westhoff. All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
